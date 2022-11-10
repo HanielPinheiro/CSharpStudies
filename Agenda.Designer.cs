6035d8e3794be8b7121021252740a1745e3f255c
Haniel Nunes Pereira Pinheiro <haniel_pereira@hotmail.com>
2022-11-10 06:59:01 -0300

    Primeiro Projeto - Agenda

---
+++ Agenda.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace EstudandoC_Sharp
+{
+    partial class Agenda
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.structurePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.gridData = new System.Windows.Forms.DataGridView();
+            this.btnPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblFiltrar = new System.Windows.Forms.Label();
+            this.cbFiltrar = new System.Windows.Forms.ComboBox();
+            this.structurePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).BeginInit();
+            this.btnPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // structurePanel
+            // 
+            this.structurePanel.ColumnCount = 1;
+            this.structurePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.structurePanel.Controls.Add(this.gridData, 0, 0);
+            this.structurePanel.Controls.Add(this.btnPanel, 0, 1);
+            this.structurePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.structurePanel.Location = new System.Drawing.Point(0, 0);
+            this.structurePanel.Name = "structurePanel";
+            this.structurePanel.RowCount = 2;
+            this.structurePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.structurePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.structurePanel.Size = new System.Drawing.Size(378, 450);
+            this.structurePanel.TabIndex = 1;
+            // 
+            // gridData
+            // 
+            this.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridData.Location = new System.Drawing.Point(3, 3);
+            this.gridData.Name = "gridData";
+            this.gridData.Size = new System.Drawing.Size(372, 399);
+            this.gridData.TabIndex = 1;
+            this.gridData.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.alterarDadosGrid);
+            // 
+            // btnPanel
+            // 
+            this.btnPanel.ColumnCount = 3;
+            this.btnPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.61889F));
+            this.btnPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.14984F));
+            this.btnPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.23127F));
+            this.btnPanel.Controls.Add(this.button2, 0, 0);
+            this.btnPanel.Controls.Add(this.lblFiltrar, 1, 0);
+            this.btnPanel.Controls.Add(this.cbFiltrar, 2, 0);
+            this.btnPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPanel.Location = new System.Drawing.Point(3, 408);
+            this.btnPanel.Name = "btnPanel";
+            this.btnPanel.RowCount = 1;
+            this.btnPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.btnPanel.Size = new System.Drawing.Size(372, 39);
+            this.btnPanel.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Adicionar Novo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblFiltrar
+            // 
+            this.lblFiltrar.AutoSize = true;
+            this.lblFiltrar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblFiltrar.Location = new System.Drawing.Point(116, 0);
+            this.lblFiltrar.Name = "lblFiltrar";
+            this.lblFiltrar.Size = new System.Drawing.Size(76, 39);
+            this.lblFiltrar.TabIndex = 2;
+            this.lblFiltrar.Text = "Filtrar por:";
+            this.lblFiltrar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbFiltrar
+            // 
+            this.cbFiltrar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbFiltrar.FormattingEnabled = true;
+            this.cbFiltrar.Location = new System.Drawing.Point(198, 3);
+            this.cbFiltrar.Name = "cbFiltrar";
+            this.cbFiltrar.Size = new System.Drawing.Size(171, 21);
+            this.cbFiltrar.TabIndex = 3;
+            // 
+            // Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.structurePanel);
+            this.Name = "Agenda";
+            this.Text = "Agenda de Contatos";
+            this.structurePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).EndInit();
+            this.btnPanel.ResumeLayout(false);
+            this.btnPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel structurePanel;
+        private System.Windows.Forms.DataGridView gridData;
+        private System.Windows.Forms.TableLayoutPanel btnPanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblFiltrar;
+        private System.Windows.Forms.ComboBox cbFiltrar;
+    }
+}
+
---
+++ Agenda.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstudandoC_Sharp
+{
+    public partial class Agenda : Form
+    {
+        private List<Contatos> contatosAgenda = new List<Contatos>();
+        public Agenda()
+        {
+            InitializeComponent();
+            inserirContatos();
+            
+        }
+
+        private void inserirContatos()
+        {
+            contatosAgenda.Add(criarContatos(1, "João", "da Silva", "joao_daSilva@gmail.com", "11 99854-5588","A"));
+            contatosAgenda.Add(criarContatos(2, "Antonio", "Nunes", "tonin.Nunes@gmail.com", "11 93325-4477","A"));
+            contatosAgenda.Add(criarContatos(3, "Flavio", "Aroucha", "flaio.Aroucha@gmail.com", "11 98875-6547", "B"));
+            contatosAgenda.Add(criarContatos(4, "Eliana", "das Flores", "eli.Flores@gmail.com", "11 98577-3312", "C"));
+
+            gridData.DataSource = contatosAgenda.OrderBy(p => p.Name.ToString()).ToList();   
+        }
+
+        private Contatos criarContatos(int id, string name, string lastName, string email, string tel, string org)
+        {
+            Contatos contato = new Contatos();
+            contato.Id = id;
+            contato.Name = name;
+            contato.LastName = lastName;
+            contato.Email = email;
+            contato.Tel = tel;
+            contato.Organizacao = org.ToUpper();
+
+            return contato;
+        }
+
+        private void alterarDadosGrid(object sender, DataGridViewCellEventArgs e)
+        {
+            int idx = e.RowIndex;                        
+            var data = gridData.Rows[idx].Cells["Id"].Value;
+            MessageBox.Show(data.ToString());
+        }
+    }
+}
