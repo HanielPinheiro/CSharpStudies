14d3808488180c4e6bd4c09d1b8320eef9978379
Haniel Nunes Pereira Pinheiro <haniel.pereira@FDERIVS.com>
2022-12-09 16:29:38 -0300

    New Update - gitIgnoreConfig

---
+++ Alura/SQLManager/Up2DataSpreadsheetStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SQLManager
+{
+    public class Up2DataSpreadsheetStruct
+    {
+        //Coluna no arquivo; Coluna no banco
+
+        //public string ISIN { get; set; } // 3 ; Não tem
+        public string ACAO { get; set; } //2 ; 1
+        public string EMPRESA { get; set; } // 4 ; 2
+        public double NCONTATOS { get; set; } //5 ; 3
+        public double QUANTIDADE { get; set; } //6 ; 4
+        public double VALOR { get; set; } //7 ; 5
+        public double TAXA_D_Min { get; set; } //8 ; 6
+        public double TAXA_D { get; set; } //9 ; 7
+        public double TAXA_D_Max { get; set; } //10 ; 8
+        public double TAXA_T_Min { get; set; } //11 ; 9
+        public double TAXA_T { get; set; } // 12 ; 10
+        public double TAXA_T_max { get; set; }//13 ; 11
+        public DateTime DATA_ARQUIVO { get; set; }//1 ; 12
+        public DateTime DATA_ENTRADA { get; set; } //Não tem ; 13
+        public string MERCADO_NOME { get; set; } //14 ; 14
+    }
+}
---
+++ CRUD/Program.cs
@@ -0,0 +1,56 @@
+﻿using ByteBank.Holder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Execute();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Execute()
+        {
+            Client andre = new Client("André", "388012745 60", "11912344456");
+            Client maria = new Client("Maria", "005442732-0X", "11912344456");
+
+            CurrentAccount andreAccount = new CurrentAccount(15, "1010-X", andre, 100);
+            CurrentAccount mariaAccount = new CurrentAccount(17, "1010-5", maria, 350);
+
+            ShowData(andreAccount);
+            ShowBalance(mariaAccount);
+        }
+
+        static void ShowData(CurrentAccount account)
+        {
+            Console.WriteLine($"Agencie: {account.SeeAgencie()}");
+            Console.WriteLine($"Account: {account.SeeAccount()}");
+            Console.WriteLine($"Holder: {account.SeeHolder()[0]}");
+            Console.WriteLine($"Balance: R$ {String.Format("{0:0.00}", account.SeeBalance())}");
+            Console.WriteLine();
+        }
+
+        static void ShowBalance(CurrentAccount client)
+        {
+            Console.WriteLine($"Balance: R$ {String.Format("{0:0.00}", client.SeeBalance())}");
+            Console.WriteLine();
+        }
+    }
+}
+
