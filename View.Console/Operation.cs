609003fd039719c9f3664616300dc5243dd86e22
Haniel Nunes Pereira Pinheiro <haniel.pereira@FDERIVS.com>
2023-01-05 16:28:25 -0300

    new project

---
+++ View.Console/Operation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Console
+{
+    internal class Operation
+    {
+
+        public void Create()
+        {
+
+        }
+
+        public void Retrieve()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ View.Console/Text.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Console
+{
+    internal class Text
+    {
+        public void ShowMenu()
+        {
+            System.Console.WriteLine("==============================");
+            System.Console.WriteLine("=          Operations        =");
+            System.Console.WriteLine("=                            =");
+            System.Console.WriteLine("=           Welcome!         =");
+            System.Console.WriteLine("==============================");
+        }
+        public void ShowContacts()
+        {
+
+        }
+        public void Welcome()
+        {
+            System.Console.WriteLine("==============================");
+            System.Console.WriteLine("=     Contacts Scheduler     =");
+            System.Console.WriteLine("=                            =");
+            System.Console.WriteLine("=           Welcome!         =");
+            System.Console.WriteLine("==============================");
+        }
+
+    }
+}
