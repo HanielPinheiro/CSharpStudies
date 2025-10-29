f35ff5f68bb23ae7ade05e3214794adcbf1b30bc
Haniel <79666865+HanielPinheiro@users.noreply.github.com>
2025-10-29 01:51:44 -0300

    Back - v2

--- Entrevistas/practice/Back_API/Program.cs
+++ Entrevistas/practice/Back_API/Program.cs
@@ -8,18 +8,21 @@ builder.Services.AddDbContext<TodoDAO>(options => options.UseInMemoryDatabase("T
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
-app.MapGet("/todoitems", async (TodoDAO inMemoryDao) => await inMemoryDao.Todos.ToListAsync());
-app.MapGet("/todoitems/complete", async (TodoDAO db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
-app.MapGet("/todoitems/{id}", async (int id, TodoDAO inMemoryDao) => ( await inMemoryDao.Todos.FindAsync(id) ) is Todo todo ? Results.Ok(todo) : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDAO dao) =>
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", async (TodoDAO inMemoryDao) => await inMemoryDao.Todos.ToListAsync());
+todoItems.MapGet("/complete", async (TodoDAO db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
+todoItems.MapGet("/{id}", async (int id, TodoDAO inMemoryDao) => ( await inMemoryDao.Todos.FindAsync(id) ) is Todo todo ? Results.Ok(todo) : Results.NotFound());
+
+todoItems.MapPost("/", async (Todo todo, TodoDAO dao) =>
 {
     dao.Todos.Add(todo);
     await dao.SaveChangesAsync();
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDAO inMemoryDAO) =>
+todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDAO inMemoryDAO) =>
 {
     Todo? todo = await inMemoryDAO.Todos.FindAsync(id);
 
@@ -33,7 +36,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDAO inMemoryDAO
     return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDAO inMemoryDao) =>
+todoItems.MapDelete("/{id}", async (int id, TodoDAO inMemoryDao) =>
 {
     if (await inMemoryDao.Todos.FindAsync(id) is Todo todo)
     {
