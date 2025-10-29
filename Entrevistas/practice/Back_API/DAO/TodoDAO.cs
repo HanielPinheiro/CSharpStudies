45492ad96c065acc250f9dfd052a74ea86c16523
Haniel <79666865+HanielPinheiro@users.noreply.github.com>
2025-10-29 01:50:25 -0300

    back - v1

---
+++ Entrevistas/practice/Back_API/DAO/TodoDAO.cs
@@ -0,0 +1,12 @@
+﻿namespace Back_API.DAO
+{
+    using Back_API.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    class TodoDAO : DbContext
+    {
+        public TodoDAO(DbContextOptions<TodoDAO> options): base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
